2bf2c4d617169fa606b08730da34c485301fad72
Oleg Serikov <srkvoa@gmail.com>
2020-11-19 15:25:32 +0300

    Update solution_3.py

--- task_3/solution_3.py
+++ task_3/solution_3.py
@@ -1,8 +1,8 @@
 my_text = "кошка мыла\nложку, собаку, и.. виноград"
 my_text_striped = my_text.strip()
 my_text_cleaned = my_text_striped.replace(',', '')
-my_text_striped = my_text_striped.replace('.', '')
-my_text_striped = my_text_tokenized.split()
+my_text_cleaned = my_text_cleaned.replace('.', '')
+my_text_tokenized = my_text_cleaned.split()
 
 my_orig_ixes_new_values = [-1] * len(my_text)
 # your code here
